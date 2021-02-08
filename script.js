755be8006ea91c6b55e4995696335ced7fe535c9
Milan Vetnic <61905392+MilanVetnic@users.noreply.github.com>
2021-02-08 01:50:59 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+﻿function searchToggle(src, evt) {
+  let container = $(src).closest('.search-dugme');
+  if (!container.hasClass('active')) {
+    container.addClass('active');
+    evt.preventDefault();
+  } else if (container.hasClass('active') && $(src).closest('.input-holder').length == 0) {
+    container.removeClass('active');
+    // clear input
+    container.find('.search-input').val('');
+  }
+}
